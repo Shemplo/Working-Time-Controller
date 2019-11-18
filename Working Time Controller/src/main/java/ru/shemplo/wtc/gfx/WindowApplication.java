be4883d244c1419e300d1ed1a483e3851df756da
Plotnikov Andrey <shemplo@outlook.com>
2019-11-18 22:10:40 +0300

    Layout changed + dragging of window

--- Working Time Controller/src/main/java/ru/shemplo/wtc/gfx/WindowApplication.java
+++ Working Time Controller/src/main/java/ru/shemplo/wtc/gfx/WindowApplication.java
@@ -1,8 +1,12 @@
 package ru.shemplo.wtc.gfx;
 
+import java.util.Objects;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import ru.shemplo.snowball.utils.MiscUtils;
@@ -10,6 +14,8 @@ import ru.shemplo.wtc.RunWTC;
 import ru.shemplo.wtc.gfx.controllers.MainController;
 
 public class WindowApplication extends Application {
+    
+    private Point2D capture = null;
     
     @Override
     public void start (Stage stage) throws Exception {
@@ -19,6 +25,14 @@ public class WindowApplication extends Application {
         MiscUtils.<Object, MainController> cast (loader.getController ()).setStage (stage);
         url = WindowApplication.class.getResource ("/ru/shemplo/wtc/css/main.css");
         scene.getStylesheets ().add (url.toExternalForm ());
+        
+        scene.setOnMousePressed (me -> { capture = new Point2D (me.getSceneX (), me.getSceneY ()); });
+        scene.setOnMouseDragged (me -> {
+            if (!Objects.isNull (capture) && MouseButton.PRIMARY.equals (me.getButton ())) {
+                stage.setX (me.getScreenX () - capture.getX ());
+                stage.setY (me.getScreenY () - capture.getY ());
+            }
+        });
         
         stage.setTitle ("Working time controller | version " + RunWTC.VERSION);
         stage.initStyle (StageStyle.UNDECORATED);
--- Working Time Controller/src/main/java/ru/shemplo/wtc/gfx/controllers/MainController.java
+++ Working Time Controller/src/main/java/ru/shemplo/wtc/gfx/controllers/MainController.java
@@ -6,13 +6,31 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import lombok.Setter;
+import ru.shemplo.snowball.utils.MiscUtils;
 
 public class MainController implements Initializable {
     
+    @FXML private StackPane root;
+    
+    @FXML private Button startButton;
+    @FXML private Button stopButton;
+    
+    @FXML private Button settingButton;
     @FXML private Button exitButton;
+    
+    @FXML private Label projectNameValue;
+    @FXML private Label timeCounterValue;
+    @FXML private Label projectStructure;
+    
+    @FXML private ProgressBar timeTimerValue;
     
     @Setter private Stage stage;
 
@@ -21,6 +39,12 @@ public class MainController implements Initializable {
         exitButton.setOnMouseClicked (me -> {
             Optional.ofNullable (stage).ifPresent (Stage::close);
         });
+        
+        var timerParentWidthProps = MiscUtils.<Parent, HBox> cast (timeTimerValue.getParent ())
+                                  . widthProperty ();
+        timeTimerValue.minWidthProperty ().bind (timerParentWidthProps);
+        timeTimerValue.maxWidthProperty ().bind (timerParentWidthProps);
+        timeTimerValue.setProgress (0.89);
     }
 
 }
